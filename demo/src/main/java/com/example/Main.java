1525822ce475b1855974cae9cc471171d7db656f
bfdell <bf08dell@siena.edu>
2022-04-20 20:36:48 -0400

    made maven project

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,18 @@
+package com.example;
+// import de.gurkenlabs.game;
+import de.gurkenlabs.litiengine.Game;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Game.init();
+        Game.start();
+    }
+}

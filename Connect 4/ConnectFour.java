e5a5a8903771b0344de260b453b40e1483531916
spencer606 <sj06cara@siena.edu>
2022-04-21 17:00:59 -0400

    Added run method and basic window

--- Connect 4/ConnectFour.java
+++ Connect 4/ConnectFour.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.geom.*;
 
 public class ConnectFour extends MouseAdapter implements Runnable {
-
+    JPanel mainPanel;
     @Override
     public void mouseClicked(MouseEvent e) {
 
@@ -35,8 +35,21 @@ public class ConnectFour extends MouseAdapter implements Runnable {
 
     @Override
     public void run() {
-
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame("Cup Pong!!!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(800, 800));
+        mainPanel = new JPanel(new BorderLayout()){
+            @Override
+            public void paintComponent(Graphics g){
+                super.paintComponent(g);
+            }
+        };
+        frame.setResizable(false);
         
+        frame.add(mainPanel);
+        frame.pack();
+        frame.setVisible(true);
     }
     
     public static void main(String args[]){

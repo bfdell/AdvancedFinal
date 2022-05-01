83eb50c55535e0a1e5715f8cc686c2315a423762
jaclynWirth24 <ja01wirt@siena.edu>
2022-04-30 21:34:06 -0400

    Started

--- CompetitionEagle/src/main/java/csis/dptw/engine/Repainter.java
+++ CompetitionEagle/src/main/java/csis/dptw/engine/Repainter.java
@@ -1,5 +1,25 @@
 package csis.dptw.engine;
 
+import javax.swing.JComponent;
+
 public class Repainter extends Thread {
     
+
+    final int DELAY_TIME = 33 ;
+
+    JComponent container;
+
+    public Repainter(JComponent container){
+        this.container = container;
+    } 
+    public void run(){
+       while(true){
+            try {
+                sleep(DELAY_TIME);
+            } catch (InterruptedException e) {
+            }
+
+            container.repaint();
+        }
+    }
 }

f4e5395f97daa3dfdf963d532e937553bfb0b545
d21tang <d21tang@siena.edu>
2022-05-04 23:57:36 -0400

    main menu layout

--- CompetitionEagle/src/main/java/csis/dptw/App.java
+++ CompetitionEagle/src/main/java/csis/dptw/App.java
@@ -77,34 +77,35 @@ public class App implements ActionListener, Runnable {
         title = new JLabel("Competition Eagle");
         Font titleFont = new Font("SansSerif", Font.BOLD, 50);
         title.setFont(titleFont);
-        c.weightx = 0.5;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
+        c.weightx = 1.0;
+        c.weighty = 1.0;
+        c.fill = GridBagConstraints.PAGE_START;
+        c.gridx = 1;
         c.gridy = 0;
         app.add(title,c);
 
         select = new JLabel("Select A Game");
         Font selectFont = new Font("SansSerif", Font.BOLD, 20);
         select.setFont(selectFont);
-        c.fill = GridBagConstraints.HORIZONTAL;
+        c.fill = GridBagConstraints.CENTER;
         c .weightx = 0.5;
         c.gridx = 1;
-        c.gridy = 0;    
+        c.gridy = 1;    
         app.add(select,c);
 
         startConnect4 = new JButton("Connect4");
-        c.fill = GridBagConstraints.HORIZONTAL;
+        c.fill = GridBagConstraints.LINE_START;
         c.weightx = 0.5;
-        c.gridx = 2;
-        c.gridy = 0;
+        c.gridx = 0;
+        c.gridy = 1;
         app.add(startConnect4,c);
         startConnect4.addActionListener(this);
 
         startCupPong = new JButton("Cup Pong");
-        c.fill = GridBagConstraints.HORIZONTAL;
+        c.fill = GridBagConstraints.LINE_END;
         c.weightx = 0.0;
         c.gridwidth = 3;
-        c.gridx = 0;
+        c.gridx = 2;
         c.gridy = 1;
         app.add(startCupPong,c);
         startCupPong.addActionListener(this);

996ecbdd04cc9790314ce3c731788121a5d0dfb2
madelynpapa <m23papa@siena.edu>
2022-05-06 03:54:08 -0400

    finished animation javadoc

--- CompetitionEagle/src/main/java/csis/dptw/engine/Animation.java
+++ CompetitionEagle/src/main/java/csis/dptw/engine/Animation.java
@@ -1,32 +1,48 @@
 package csis.dptw.engine;
 
-public abstract class Animation extends Thread{
+public abstract class Animation extends Thread {
     public Entity entity;
     protected Game game;
 
-
     protected boolean done = false;
 
+    /**
+     * Initializes entity and game
+     * 
+     * @param entity the entity that is being animated
+     * @param game   that is currently being played
+     */
     public Animation(Entity entity, Game game) {
         this.entity = entity;
         this.game = game;
     }
 
-
-
+    /**
+     * While the condition for the animation done is not fulfilled, animate the
+     * entity
+     */
     @Override
     public void run() {
-        while(!done()) {
+        while (!done()) {
             animation();
         }
     }
 
+    /**
+     * Modifies Entity's values for animation
+     */
     public abstract void animation();
-    
+
+    /**
+     * @return if animation is done
+     */
     public boolean done() {
         return true;
     }
 
+    /**
+     * @return entity that is being animated
+     */
     public Entity getEntity() {
         return entity;
     }

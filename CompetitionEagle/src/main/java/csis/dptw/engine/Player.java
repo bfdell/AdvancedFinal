82fd8cdfa9503d6dfc8c6a71a7b3950ae815304b
madelynpapa <m23papa@siena.edu>
2022-05-06 04:02:40 -0400

    finished player javadoc

--- CompetitionEagle/src/main/java/csis/dptw/engine/Player.java
+++ CompetitionEagle/src/main/java/csis/dptw/engine/Player.java
@@ -5,7 +5,10 @@ package csis.dptw.engine;
 public class Player {
   
     public final int PLAYER_NUM;
-    // public final int NAME;
+    /**
+     * initializes the playernum
+     * @param playerNum number of the player
+     */
     public Player(int playerNum) {
         PLAYER_NUM = playerNum;
 
